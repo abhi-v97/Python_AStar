244ed5ad18c21cdde7576dcad2058eaa30224cf4
abhiv-97 <v.abhishek@outlook.com>
2021-03-20 16:34:36 +0100

    algorithm code done

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ class Node():
     start = []
     end = []
 
+    blockade = []
+
     start_node = None
     end_node = None
 
@@ -21,10 +23,17 @@ class Node():
         self.h = 0
         self.f = 0
 
+        self.parent = None
+
         Node.all.append(self)
     
     def __repr__(self):
         return f"Cell({self.x},{self.y})"
+    
+    def __eq__(self, other):
+        return (self.x == other.x and self.y == other.y)
+
+
 
     def create_node(self, location):
         node = Button(location,
@@ -46,27 +55,29 @@ class Node():
             
             Node.mode = 1
             self.node.configure(bg='green')
-            
-            #print(Node.all)
-            
 
         
         elif Node.mode == 1:
 
             Node.end.append(x)
             Node.end.append(y)
+            Node.end_node = self.get_node(x, y)
             
             Node.mode = 2
             self.node.configure(bg='red')
+            
+            #print(self.heuristic(Node.start_node, Node.end_node))
 
         elif Node.mode == 2:
             self.node.configure(bg='black')
+            Node.blockade.append(self.get_node(x, y))
+            print(Node.blockade)
             
             root.bind('<Return>', self.a_star)
             
     
     def heuristic(self, node1, node2):
-        result = abs(node1[0] - node2[0]) + abs(node1[1]-node2[1])
+        result = abs(node1.x - node2.x) + abs(node1.y-node2.y)
         return result
     
 
@@ -80,38 +91,107 @@ class Node():
             if node.f == f_score:
                 return node
     
+    @property
+    def neighbour_list(self):
+        node_list = [
+            self.get_node(self.x - 1, self.y - 1),
+            self.get_node(self.x - 1, self.y),
+            self.get_node(self.x - 1, self.y + 1),
+            self.get_node(self.x, self.y - 1),
+            self.get_node(self.x + 1, self.y - 1),
+            self.get_node(self.x + 1, self.y),
+            self.get_node(self.x + 1, self.y + 1),
+            self.get_node(self.x, self.y + 1),
+        ]
+        node_list = [node for node in node_list if node is not None]
+
+        return node_list
+    
     def a_star(self, event):
         open_list = []
-        closed = []
+        closed_list = []
+        #closed_list = closed_list + Node.blockade
         current = 0
+        Node.start_node.g = Node.start_node.h = Node.start_node.f = 0
+        Node.end_node.g = Node.end_node.h = Node.end_node.f = 0
 
         #open_list.append(start_node)
 
-        Node.start_node.f = self.heuristic(Node.start, Node.end)
-        print(Node.start_node.f)
-        
-        #while len(open_list) > 0:
-        #current = f_score.index(5)
-        #self.get_node(min(f_score))
-        #print("Cur", current)
-            #if current == Node.end:
-                #break
-                
-            
+        #Node.start_node.f = self.heuristic(Node.start_node, Node.end_node)
+        #print(self.get_node_f(Node.start_node.f))
 
-
-        
-        
-
+        open_list.append(Node.start_node)
 
     
+        while len(open_list) > 0:
+
+            current_node = open_list[0]
+            current_index = 0
             
 
+            for index, item in enumerate(open_list):
+                print(current_node, item, item.f, current_node.f)
+                if item.f < current_node.f:
+                    current_node = item
+                    current_index = index
+
+      
+            open_list.pop(current_index)
+            #print("OPEN???", open_list)
+            #print("close???", closed_list)
+            closed_list.append(current_node)
+
+            # check if we reached the end
+
+            if current_node == Node.end_node:
+                print("BOO")
+                total_path = []
+                current = current_node
+                Node.start_node.parent = None
+
+                while current is not None:
+                    root.update_idletasks()
+                    self.node.config(bg = "blue")
+                    total_path.append(current)
+                    current = current.parent
+                
+
+                print (total_path)
+
+                
+                
+                return True
+            
+            #print(current_node.x, type(current_node), type(Node.start_node))
+            neighbour = current_node.neighbour_list
+            
+            for i in neighbour:
+                
+                if i in closed_list or i in Node.blockade:
+                    continue
+            
+                i.g = current_node.g + 1
+
+                
+                
+                i.h = self.heuristic(i, Node.end_node)
+                i.f = i.g + i.h
+
+                for k in open_list:
+                    if i == k and i.g > k.g:
+                        continue
+                
+                
+                i.parent = current_node
+                print(f"adding {i} to {open_list} because its not in {closed_list}", current_node)
+                open_list.append(i)
+
+        print("Fail!")
 
 
-
-
-
+                     
+ 
+        
 root = Tk()
 root.title("A* Algorithm")
 
